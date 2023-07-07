3db34c11700ae2b90b471823b855ed0095aeb99e
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-07-07 22:15:58 +0400

    adding stats page

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ChartsContainer from "../../components/ChartsContainer";
+import StatsContainer from "../../components/StatsContainer";
+import { showStats } from "../../features/allJobs/allJobsSlice";
 
 const Stats = () => {
-  return <div>Stats</div>;
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
+
+  return (
+    <div>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </div>
+  );
 };
 
 export default Stats;
